58a6d5869682a253e5d578903cbb96046b043902
coupsjeong95 <coupsjeong95@gmail.com>
2026-04-13 17:18:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -69,7 +69,6 @@ function cardHTML(f, idx) {
   <div class="card-mid">
     <p class="card-summary">${esc(f.summary)}</p>
     <span class="card-platform">${f.platform}</span> </div>
-  </div>
   <div class="card-tags">${tagsStr}</div>
 </article>`;
 }
